9f2c0f8c4cac2a0e52294413ace3d6ba69b39b99
Mymanat <mymanat.mohammed@polymtl.ca>
2025-03-25 03:07:41 -0400

    dataframe to predict mvp completed

--- PredictMVP.py
+++ PredictMVP.py
@@ -126,8 +126,10 @@ st.dataframe(df2)
 
 df_players_combined = pd.merge(df1, df2, on=['Year', 'Player', 'Team'], how='inner')
 df_players_combined['Team'] = df_players_combined['Team'].map(team_mapping)
+df_players_combined.rename(columns={col: f'Player-{col}' for col in df_players_combined.columns.to_list()[4:]}, inplace=True)
 st.title('Player stats per game + advanced stats per game')
 st.dataframe(df_players_combined)
+
 
 @st.cache_data
 def team_stats():
@@ -188,12 +190,9 @@ st.title('Team advanced stats per game')
 st.dataframe(df4)
 
 df_teams_combined = pd.merge(df3, df4, on=['Year', 'Team'], how='inner')
+df_teams_combined.rename(columns={col: f'Team-{col}' for col in df_teams_combined.columns.to_list()[2:]}, inplace=True)
 st.title('Team stats per game + advanced stats per game')
 st.dataframe(df_teams_combined)
-
-# df_teams_combined_final = pd.merge(df_teams_combined, df5, on=['Year', 'Team'], how='inner')
-# st.title('Player stats per game + advanced stats per game + Team standings')
-# st.dataframe(df_teams_combined_final)
 
 df_teams_players_combined = pd.merge(df_players_combined, df_teams_combined, on=['Year', 'Team'], how='inner')
 st.title('Player all stats + Team all stats')
@@ -203,16 +202,38 @@ missing_players_teams = df_players_combined[~df_players_combined.set_index(['Yea
     df_teams_players_combined.set_index(['Year', 'Team']).index
 )]
 
-# for i in range(len(missing_players_teams)):
-#     for j in range(len(df_teams_players_combined)):
-#         if missing_players_teams[['Year', 'Player']].iloc[i].equals(df_teams_players_combined[['Year', 'Player']].iloc[j]):
-#             st.dataframe(df_teams_players_combined.iloc[j])
-#             st.dataframe(missing_players_teams.iloc[i])
+st.dataframe(missing_players_teams)
 
-matches = missing_players_teams.merge(df_teams_players_combined, on=['Year', 'Player'], how='inner')
+new_df = pd.merge(df_teams_players_combined, missing_players_teams, how='outer')
 
-# Display matches
-st.dataframe(matches)
+for index,row in missing_players_teams.iterrows():
+    year = row['Year']
+    player = row['Player']
+    team = row['Team']
+
+    player_rows = new_df[(new_df['Year']==year)&(new_df['Player']==player)&(~new_df['Team'].isin(['2TM', '3TM', '4TM', '5TM']))]
+
+    if not player_rows.empty:
+        games = pd.to_numeric(player_rows['Player-G'], errors='coerce')
+        if games.sum() == 0:
+            continue
+        weights = games / games.sum()
+
+        team_stat_cols = [col for col in new_df.columns if col.startswith('Team-')]
+
+        numeric_stats = player_rows[team_stat_cols].apply(pd.to_numeric, errors='coerce')
+        weighted_avg = (numeric_stats.T * weights).T.sum()
+
+        for col in team_stat_cols:
+            new_df.loc[(new_df['Year']==year) & (new_df['Player']==player) & (new_df['Team']==team), col] = weighted_avg[col]
+
+
+new_df['Player-Awards'] = new_df['Player-Awards'].fillna('').astype(str)
+new_df['MVP'] = new_df['Player-Awards'].apply(lambda x: 1 if 'MVP-1' in x.split(',') else 0)
+
+st.title("NEW DF")
+st.dataframe(new_df)
+
 
 
 
