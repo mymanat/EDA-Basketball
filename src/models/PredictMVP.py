31740f45eb38802c0fe64b07b8b3de97149aa2a6
Mymanat <mymanat.mohammed@polymtl.ca>
2025-05-06 14:09:15 -0400

    added mvp prediction to MVP.py

--- src/models/PredictMVP.py
+++ src/models/PredictMVP.py
@@ -3,12 +3,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from urllib.request import Request, urlopen
-import time
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from src.constants import TEAM_MAPPING, FIRST_YEAR_MVP, CURRENT_YEAR, PREVIOUS_YEAR
+from src.constants import TEAM_MAPPING, CURRENT_YEAR
 from src.utils import scrape
 
 st.set_page_config(page_title='Basketball Statistics Explorer', layout='wide')
@@ -21,7 +18,8 @@ all_seasons_players['MVP'] = all_seasons_players['Player-Awards'].apply(lambda x
 st.title("All players since 1956")
 st.dataframe(all_seasons_players)
 
-mvp_training_data = all_seasons_players[all_seasons_players['Year'] >= 1980].drop(['Player', 'Age', 'Team', 'Player-Pos', 'Player-Awards', 'Player-GS', 'Team-MP'], axis=1).reset_index(drop=True)
+#MVP training data 
+mvp_training_data = all_seasons_players[all_seasons_players['Year'] >= 2014].drop(['Player', 'Age', 'Team', 'Player-Pos', 'Player-Awards', 'Player-GS', 'Team-MP'], axis=1).reset_index(drop=True)
 for i in range(0, len(mvp_training_data)):
     if mvp_training_data.loc[i, 'Player-FGA'] == 0:
         mvp_training_data.loc[i, 'Player-FG%'] = 0
@@ -72,8 +70,11 @@ smote = SMOTE(random_state=101)
 X_smote, y_smote = smote.fit_resample(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X_smote,y_smote,test_size=0.2, random_state=101)
 
+import joblib
+
 model = LogisticRegression()
 model.fit(X_train, y_train)
+joblib.dump(model, 'src/models/linear_regression_model.pkl')
 predictions = model.predict(X_test)
 print("Logistic Regression after over-sampling")
 print(classification_report(y_test, predictions))
@@ -104,114 +105,6 @@ print(classification_report(y_test, xgb_predict))
 print(confusion_matrix(y_test, xgb_predict))
 print(roc_auc_score(y_test, xgb_model.predict_proba(X_test)[:,1]))
 
-#Creating DF for current season
-current_season_players_csv ='data/raw/current_season_players.csv'
-if os.path.exists(current_season_players_csv):
-    current_season_players = pd.read_csv(current_season_players_csv)
-else:
-    df1 = scrape.player_stats(CURRENT_YEAR,CURRENT_YEAR)
-    df2 = scrape.player_advanced_stats(CURRENT_YEAR,CURRENT_YEAR)
-    df_players_combined = pd.merge(df1, df2, on=['Year', 'Player', 'Team'], how='inner')
-    df_players_combined['Team'] = df_players_combined['Team'].map(TEAM_MAPPING)
-    df_players_combined.rename(columns={col: f'Player-{col}' for col in df_players_combined.columns.to_list()[4:]}, inplace=True)
-    df3 = scrape.team_stats(CURRENT_YEAR,CURRENT_YEAR)
-    df4 = scrape.team_advanced_stats(CURRENT_YEAR,CURRENT_YEAR)
-
-    df_teams_combined = pd.merge(df3, df4, on=['Year', 'Team'], how='inner')
-    df_teams_combined.rename(columns={col: f'Team-{col}' for col in df_teams_combined.columns.to_list()[2:]}, inplace=True)
-
-    df_teams_players_combined = pd.merge(df_players_combined, df_teams_combined, on=['Year', 'Team'], how='inner')
-
-    missing_players_teams = df_players_combined[~df_players_combined.set_index(['Year', 'Team']).index.isin(
-        df_teams_players_combined.set_index(['Year', 'Team']).index
-    )]
-
-    new_df = pd.merge(df_teams_players_combined, missing_players_teams, how='outer')
-
-    for index,row in missing_players_teams.iterrows():
-        year = row['Year']
-        player = row['Player']
-        team = row['Team']
-
-        player_rows = new_df[(new_df['Year']==year)&(new_df['Player']==player)&(~new_df['Team'].isin(['2TM', '3TM', '4TM', '5TM']))]
-
-        if not player_rows.empty:
-            games = pd.to_numeric(player_rows['Player-G'], errors='coerce')
-            if games.sum() == 0:
-                continue
-            weights = games / games.sum()
-
-            team_stat_cols = [col for col in new_df.columns if col.startswith('Team-')]
-
-            numeric_stats = player_rows[team_stat_cols].apply(pd.to_numeric, errors='coerce')
-            weighted_avg = (numeric_stats.T * weights).T.sum().round(3)
-
-            for col in team_stat_cols:
-                new_df.loc[(new_df['Year']==year) & (new_df['Player']==player) & (new_df['Team']==team), col] = weighted_avg[col]
-
-    cols_to_multiply = [
-        'Player-FG%', 'Player-FT%', 'Player-2P%', 'Player-3P%', 'Player-TS%', 'Player-eFG%',
-        'Team-FG%', 'Team-3P%', 'Team-2P%', 'Team-FT%', 'Team-TS%', 'Team-eFG%_O', 
-        'Team-eFG%_D'
-    ]
-    new_df[cols_to_multiply] = new_df[cols_to_multiply].apply(pd.to_numeric, errors='coerce')
-    new_df[cols_to_multiply] = new_df[cols_to_multiply].multiply(100).round(3)
-    new_df.to_csv(current_season_players_csv, index=False)
-
-st.title(f"{CURRENT_YEAR} season players")
-st.dataframe(current_season_players)
-
-current = current_season_players.drop(['Player', 'Age', 'Team', 'Player-Pos', 'Player-Awards', 'Player-GS', 'Team-MP'], axis=1).reset_index(drop=True)
-for i in range(0, len(current)):
-    if current.loc[i, 'Player-FGA'] == 0:
-        current.loc[i, 'Player-FG%'] = 0
-        current.loc[i, 'Player-eFG%'] = 0
-        current.loc[i, 'Player-FTr'] = 0
-        current.loc[i, 'Player-3PAr'] = 0
-    if current.loc[i, 'Player-2PA'] == 0:
-        current.loc[i, 'Player-2P%'] = 0
-    if current.loc[i, 'Player-3PA'] == 0:
-        current.loc[i, 'Player-3P%'] = 0
-    if current.loc[i, 'Player-FTA'] == 0:
-        current.loc[i, 'Player-FT%'] = 0
-    if current.loc[i, 'Player-FGA'] == 0 or current.loc[i, 'Player-FTA'] == 0:
-        current.loc[i, 'Player-TS%'] = 0
-    if (current.loc[i, 'Player-FGA'] == 0) and (current.loc[i, 'Player-FTA'] == 0) and (current.loc[i, 'Player-TOV'] == 0):
-        current.loc[i, 'Player-TOV%'] = 0
-    if current.loc[i, 'Player-MP'] == 0:
-        current.loc[i, 'Player-PER'] = 0
-        current.loc[i, 'Player-WS/48'] = 0
-        current.loc[i, 'Player-AST%'] = 0
-        current.loc[i, 'Player-STL%'] = 0
-        current.loc[i, 'Player-TRB%'] = 0
-        current.loc[i, 'Player-ORB%'] = 0
-        current.loc[i, 'Player-DRB%'] = 0
-        current.loc[i, 'Player-BLK%'] = 0
-        current.loc[i, 'Player-OBPM'] = 0
-        current.loc[i, 'Player-DBPM'] = 0
-        current.loc[i, 'Player-BPM'] = 0
-        current.loc[i, 'Player-USG%'] = 0
-        current.loc[i, 'Player-VORP'] = 0
-
-current_reordered = current[X.columns]
-current_reordered = current_reordered.apply(pd.to_numeric, errors='coerce')
-st.dataframe(current_reordered)
-
-# pred_current_year = model.predict(current_reordered)
-# prob_current_year = model.predict_proba(current_reordered)[:,1]
-# st.write("Number of MVP predictions according to logistic regression:", sum(pred_current_year))
-# current_reordered['MVP Prediction'] = pred_current_year
-# current_reordered['MVP Prediction Probability'] = prob_current_year
-# top_mvp_candidates = current_reordered.sort_values(by='MVP Prediction Probability', ascending=False ).head(20)
-# st.dataframe(top_mvp_candidates)
-# importances = model.coef_[0]
-# feature_names = X.columns
-# sorted_indices = np.argsort(importances)[-10:]
-# plt.barh(range(len(sorted_indices)), importances[sorted_indices], align='center')
-# plt.yticks(range(len(sorted_indices)), feature_names[sorted_indices])
-# st.pyplot(plt.gcf())
-
-
 # pred_current_year = rfc.predict(current_reordered)
 # prob_current_year = rfc.predict_proba(current_reordered)[:,1]
 # st.write("Number of MVP predictions according to RandomForestClassifier:", sum(pred_current_year))
@@ -225,7 +118,6 @@ st.dataframe(current_reordered)
 # plt.barh(range(len(sorted_indices)), importances[sorted_indices], align='center')
 # plt.yticks(range(len(sorted_indices)), feature_names[sorted_indices])
 # st.pyplot(plt.gcf())
-
 
 # pred_current_year = model_svc.predict(current_reordered)
 # prob_current_year = model_svc.predict_proba(current_reordered)[:,1]
@@ -241,17 +133,16 @@ st.dataframe(current_reordered)
 # plt.yticks(range(len(sorted_indices)), feature_names[sorted_indices])
 # st.pyplot(plt.gcf())
 
-
-pred_current_year = xgb_model.predict(current_reordered)
-prob_current_year = xgb_model.predict_proba(current_reordered)[:,1]
-st.write("Number of MVP predictions according to XGBoost:", sum(pred_current_year))
-current_reordered['MVP Prediction'] = pred_current_year
-current_reordered['MVP Prediction Probability'] = prob_current_year
-top_mvp_candidates = current_reordered.sort_values(by='MVP Prediction Probability', ascending=False ).head(20)
-st.dataframe(top_mvp_candidates)
-importances = xgb_model.feature_importances_
-feature_names = X.columns
-sorted_indices = np.argsort(importances)[-10:]
-plt.barh(range(len(sorted_indices)), importances[sorted_indices], align='center')
-plt.yticks(range(len(sorted_indices)), feature_names[sorted_indices])
-st.pyplot(plt.gcf())
+# pred_current_year = xgb_model.predict(current_reordered)
+# prob_current_year = xgb_model.predict_proba(current_reordered)[:,1]
+# st.write("Number of MVP predictions according to XGBoost:", sum(pred_current_year))
+# current_reordered['MVP Prediction'] = pred_current_year
+# current_reordered['MVP Prediction Probability'] = prob_current_year
+# top_mvp_candidates = current_reordered.sort_values(by='MVP Prediction Probability', ascending=False ).head(20)
+# st.dataframe(top_mvp_candidates)
+# importances = xgb_model.feature_importances_
+# feature_names = X.columns
+# sorted_indices = np.argsort(importances)[-10:]
+# plt.barh(range(len(sorted_indices)), importances[sorted_indices], align='center')
+# plt.yticks(range(len(sorted_indices)), feature_names[sorted_indices])
+# st.pyplot(plt.gcf())
